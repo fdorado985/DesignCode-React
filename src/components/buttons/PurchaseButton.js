afefe2d88bae21f715aafcb6b6de72736a0c4c70
Juan Dorado <fdorado985@gmail.com>
2020-12-06 12:49:50 -0600

    Create a new React component, that will be the purchase button

---
+++ src/components/buttons/PurchaseButton.js
@@ -0,0 +1,19 @@
+import React from "react"
+import styled from "styled-components"
+
+function PurchaseButton() {
+  return (
+    <Wrapper>
+      <Title>Get Pro Access</Title>
+      <Subtitle>$19 per month</Subtitle>
+    </Wrapper>
+  )
+}
+
+export default PurchaseButton
+
+const Wrapper = styled.div``
+
+const Title = styled.p``
+
+const Subtitle = styled.p``
