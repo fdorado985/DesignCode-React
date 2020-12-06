d6d2c3b800295dcefb7ce028f3e8acad5ae0cd5c
Juan Dorado <fdorado985@gmail.com>
2020-12-06 12:39:53 -0600

    Add a Global Style with the media to detech prefered appearance on system

--- src/components/styles/GlobalStyle.js
+++ src/components/styles/GlobalStyle.js
@@ -4,5 +4,9 @@ import { themes } from "./ColorStyles"
 export const GlobalStyle = createGlobalStyle`
   body {
     background: ${themes.light.backgroundColor};
+
+    @media (prefers-color-scheme: dark) {
+      background: ${themes.dark.backgroundColor};
+    }
   }
 `
