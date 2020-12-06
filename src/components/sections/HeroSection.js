97985a2b224f3e6482e5df9a9bd8b08071535973
Juan Dorado <fdorado985@gmail.com>
2020-12-06 02:09:39 -0600

    Remove the link element and the textwrapper make it to be styled as a grid with a 30px gap and line height of 130% for the description text

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 import styled from "styled-components"
 
 function HeroSection() {
@@ -8,12 +7,14 @@ function HeroSection() {
       <ContentWrapper>
         <TextWrapper>
           <img src="/images/logos/logo.svg" alt="logo" />
-          <Title>Design and code React apps</Title>
+          <Title>
+            Design
+            <br /> and code React apps
+          </Title>
           <Description>
             Don’t skip design. Learn design and code by building real apps with
             React and Swift. Complete courses about the best tools.
           </Description>
-          <Link to="/page-2/">Go to page 2</Link> <br />
         </TextWrapper>
       </ContentWrapper>
     </Wrapper>
@@ -34,13 +35,17 @@ const ContentWrapper = styled.div`
 
 const TextWrapper = styled.div`
   max-width: 360px;
+  display: grid;
+  gap: 30px;
 `
 
 const Title = styled.h1`
   font-weight: bold;
   font-size: 60px;
+  color: white;
 `
 
 const Description = styled.p`
   font-size: 17px;
+  line-height: 130%;
 `
