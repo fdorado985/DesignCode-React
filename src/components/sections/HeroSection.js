275e0c4397c8c555bf25338c71a130794def0c4a
Juan Dorado <fdorado985@gmail.com>
2020-12-06 02:04:34 -0600

    Wrap the content on TextWrapper, Title and Description using styled components

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -6,13 +6,15 @@ function HeroSection() {
   return (
     <Wrapper>
       <ContentWrapper>
-        <img src="/images/logos/logo.svg" alt="logo" />
-        <h1>Design and code React apps</h1>
-        <p>
-          Don’t skip design. Learn design and code by building real apps with
-          React and Swift. Complete courses about the best tools.
-        </p>
-        <Link to="/page-2/">Go to page 2</Link> <br />
+        <TextWrapper>
+          <img src="/images/logos/logo.svg" alt="logo" />
+          <Title>Design and code React apps</Title>
+          <Description>
+            Don’t skip design. Learn design and code by building real apps with
+            React and Swift. Complete courses about the best tools.
+          </Description>
+          <Link to="/page-2/">Go to page 2</Link> <br />
+        </TextWrapper>
       </ContentWrapper>
     </Wrapper>
   )
@@ -28,4 +30,17 @@ const ContentWrapper = styled.div`
   max-width: 1234px;
   margin: 0 auto;
   padding: 200px 30px;
+`
+
+const TextWrapper = styled.div`
+  max-width: 360px;
+`
+
+const Title = styled.h1`
+  font-weight: bold;
+  font-size: 60px;
+`
+
+const Description = styled.p`
+  font-size: 17px;
 `
