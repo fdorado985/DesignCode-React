aeb7845108a429f0cce2da417648ebb5ed139744
Juan Dorado <fdorado985@gmail.com>
2020-12-06 01:45:22 -0600

    Make a prettier format

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -3,10 +3,15 @@ import { Link } from "gatsby"
 
 function HeroSection() {
   return (
-    <><img src="/images/logos/logo.svg" alt="logo" />
-    <h1>Design and code React apps</h1>
-    <p>Don’t skip design. Learn design and code by building real apps with React and Swift. Complete courses about the best tools.</p>
-    <Link to="/page-2/">Go to page 2</Link> <br /></>
+    <>
+      <img src="/images/logos/logo.svg" alt="logo" />
+      <h1>Design and code React apps</h1>
+      <p>
+        Don’t skip design. Learn design and code by building real apps with
+        React and Swift. Complete courses about the best tools.
+      </p>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+    </>
   )
 }
 
