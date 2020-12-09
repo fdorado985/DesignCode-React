9f82d02dcd8f021fff72764697de469250beb4b2
Juan Dorado <fdorado985@gmail.com>
2020-12-09 00:40:51 -0600

    Create the structure for the animations component

---
+++ src/components/animations/MockupAnimation.js
@@ -0,0 +1,9 @@
+import { func } from "prop-types"
+import React from "react"
+import styled from "styled-components"
+
+export default function MockupAnimation() {
+  return <Wrapper></Wrapper>
+}
+
+const Wrapper = styled.div``
