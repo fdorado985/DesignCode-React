0ecbcca38d4ffd337da568148c8f21d09d2273cc
Juan Dorado <fdorado985@gmail.com>
2020-12-12 17:48:10 -0600

    Use an styled component and add its style

--- src/components/tooltips/MenuTooltip.js
+++ src/components/tooltips/MenuTooltip.js
@@ -1,7 +1,24 @@
 import React, { useState } from "react"
+import styled from "styled-components"
 
 export default function MenuTooltip() {
   const [isOpen, setIsOpen] = useState(false)
 
-  return <div onClick={() => setIsOpen(true)}>{isOpen ? "Open" : "Closed"}</div>
+  return (
+    <Wrapper onClick={() => setIsOpen(true)}>
+      {isOpen ? "Open" : "Closed"}
+    </Wrapper>
+  )
 }
+
+const Wrapper = styled.div`
+  position: absolute;
+  top: 60px;
+  right: 30px;
+  background: rgba(15, 14, 71, 0.3);
+  box-shadow: 0px 50px 100px rgba(0, 0, 0, 0.25),
+    inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.2);
+  backdrop-filter: blur(40px);
+  border-radius: 20px;
+  padding: 20px;
+`
