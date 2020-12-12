53c204c89cfd8b3a046b8088a7a5132a5390214c
Juan Dorado <fdorado985@gmail.com>
2020-12-12 00:25:22 -0600

    Refactor the Menu Item Link to a MenuButton

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,7 +1,8 @@
 import React from "react"
-import { Link } from "gatsby"
+
 import styled from "styled-components"
 import { menuData } from "../../data/menuData"
+import MenuButton from "../buttons/MenuButton"
 
 export default function Header() {
   return (
@@ -9,12 +10,7 @@ export default function Header() {
       <img src="/images/logos/logo.svg" />
       <MenuWrapper count={menuData.length}>
         {menuData.map((item, index) => (
-          <Link to={item.link} key={index}>
-            <MenuItem title={item.title}>
-              <img src={item.icon} alt={item.title} />
-              {item.title}
-            </MenuItem>
-          </Link>
+          <MenuButton item={item} key={index} />
         ))}
       </MenuWrapper>
     </Wrapper>
@@ -36,21 +32,4 @@ const MenuWrapper = styled.div`
   display: grid;
   gap: 30px;
   grid-template-columns: repeat(${props => props.count}, auto);
-`
-
-const MenuItem = styled.div`
-  color: rgba(255, 255, 255, 0.7);
-  display: grid;
-  grid-template-columns: 24px auto;
-  gap: ${props => (props.title ? "10px" : "0px")};
-  align-items: center;
-  padding: 10px;
-  transition: 0.5s ease-out;
-  border-radius: 10px;
-
-  :hover {
-    background: rgba(255, 255, 255, 0.1);
-    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1),
-      inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.2);
-  }
 `
