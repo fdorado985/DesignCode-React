fd54a083876b4eac9e5f47d615aa08fce310b7f9
Juan Dorado <fdorado985@gmail.com>
2020-12-12 17:44:55 -0600

    Change the state using the onClick listener

--- src/components/tooltips/MenuTooltip.js
+++ src/components/tooltips/MenuTooltip.js
@@ -3,5 +3,5 @@ import React, { useState } from "react"
 export default function MenuTooltip() {
   const [isOpen, setIsOpen] = useState(false)
 
-  return <div>Button</div>
+  return <div onClick={() => setIsOpen(true)}>{isOpen ? "Open" : "Closed"}</div>
 }
