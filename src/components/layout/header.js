d146746453592076fe4b4d2d850a1f2e5660d846
Juan Dorado <fdorado985@gmail.com>
2020-12-11 20:52:00 -0600

    add a transition and move the border to the MenuItem instead of just hover

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -54,11 +54,12 @@ const MenuItem = styled.div`
   gap: 10px;
   align-items: center;
   padding: 10px;
+  transition: 0.5s ease-out;
+  border-radius: 10px;
 
   :hover {
     background: rgba(255, 255, 255, 0.1);
     box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1),
       inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.2);
-    border-radius: 10px;
   }
 `
